9d19b0c0b14d538063eeabe9c6eb3f718085703a
Ba-Zen <bazenatlas@gmail.com>
2019-06-17 19:34:13 -0400

    cleaned up repair and succeed test code

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -3,21 +3,14 @@ const enhancer = require('./enhancer.js');
 
 describe('enhancer.js', () => {
   describe('repair() method', () => {
-    it('should accept an item and return a new item with the durability restored to 100', () => {
-      const original = {
-        name: 'sword',
-        durability: 20,
-        enhancement: 18
-      };
-      const expected = {
-        name: 'sword',
-        durability: 100,
+    it('restores durability to 100', () => {
+      const item = {
+        name: 'item one',
+        durability: 90,
         enhancement: 18
       };
 
-      const item = enhancer.repair(original);
-      expect(item).toEqual(expected);
-      //console.log(item);
+      expect(enhancer.repair(item).durability).toBe(100);
     });
   });
 });
@@ -25,26 +18,33 @@ describe('enhancer.js', () => {
 describe('enhancer.js', () => {
   describe('succeed() method', () => {
     it('should increase item enhancement by 1 if less than 20', () => {
-      const original = {
+      const item = {
         name: 'sword',
         durability: 100,
         enhancement: 18
       };
-      const expected = {
-        name: 'sword',
-        durability: 100,
-        enhancement: 19
-      };
 
-      const maxEnhanced = {
-        name: 'sword',
-        durability: 100,
-        enhancement: 20
-      };
+      expect(enhancer.succeed(item).enhancement).toBe(19);
+      expect(enhancer.succeed({ enhancement: 10 }).enhancement).toBe(11);
+      expect(enhancer.succeed({ enhancement: 19 }).enhancement).toBe(20);
+      expect(enhancer.succeed({ enhancement: 20 }).enhancement).toBe(20);
 
-      const item = enhancer.succeed(original);
-      expect(item).toEqual(expected);
-      expect(enhancer.succeed(maxEnhanced).enhancement).toBe(20);
+      //expect(enhancer.fail({ enhancement: 20 }).enhancement).toEqual(19);
+
+      //   const expected = {
+      //     name: 'sword',
+      //     durability: 100,
+      //     enhancement: 19
+      //   };
+
+      //   const maxEnhanced = {
+      //     name: 'sword',
+      //     durability: 100,
+      //     enhancement: 20
+      //   };
+
+      //   const item = enhancer.succeed(original);
+      //   expect(item).toEqual(expected);
       //console.log(item);
       //console.log(maxEnhanced);
     });
@@ -105,8 +105,10 @@ describe('enhancer.js', () => {
       };
 
       expect(enhancer.fail({ enhancement: 17 }).enhancement).toEqual(16);
+      expect(enhancer.fail({ enhancement: 20 }).enhancement).toEqual(19);
+
       const item3 = enhancer.fail(original3);
-      //expect(item3).toEqual(expected3);
+      expect(item3).toEqual(expected3);
       //console.log(item3);
     });
   });

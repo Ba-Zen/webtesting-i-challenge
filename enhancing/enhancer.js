91160187ad3571fe0a0c028e901301b1174e3a64
Ba-Zen <bazenatlas@gmail.com>
2019-06-17 18:19:35 -0400

    mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,19 +2,37 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    return { ...item, enhancement: item.enhancement + 1 };
+  } else {
+    return { ...item };
+  }
 }
 
 function fail(item) {
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 };
+  } else if (item.enhancement >= 15) {
+    if (item.enhancement > 16) {
+      return {
+        ...item,
+        enhancement: item.enhancement - 1,
+        durability: item.durability - 10
+      };
+    }
+  } else {
+    return { ...item, durability: item.durability - 10 };
+  }
+
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,113 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer.js', () => {
+  describe('repair() method', () => {
+    it('should accept an item and return a new item with the durability restored to 100', () => {
+      const original = {
+        name: 'sword',
+        durability: 20,
+        enhancement: 18
+      };
+      const expected = {
+        name: 'sword',
+        durability: 100,
+        enhancement: 18
+      };
+
+      const item = enhancer.repair(original);
+      expect(item).toEqual(expected);
+      //console.log(item);
+    });
+  });
+});
+
+describe('enhancer.js', () => {
+  describe('succeed() method', () => {
+    it('should increase item enhancement by 1 if less than 20', () => {
+      const original = {
+        name: 'sword',
+        durability: 100,
+        enhancement: 18
+      };
+      const expected = {
+        name: 'sword',
+        durability: 100,
+        enhancement: 19
+      };
+
+      const maxEnhanced = {
+        name: 'sword',
+        durability: 100,
+        enhancement: 20
+      };
+
+      const item = enhancer.succeed(original);
+      expect(item).toEqual(expected);
+      expect(enhancer.succeed(maxEnhanced).enhancement).toBe(20);
+      //console.log(item);
+      //console.log(maxEnhanced);
+    });
+  });
+});
+
+describe('enhancer.js', () => {
+  describe('fail() method', () => {
+    it('should subtract durability by 5 if less than 15', () => {
+      const original = {
+        name: 'sword',
+        durability: 100,
+        enhancement: 14
+      };
+      const expected = {
+        name: 'sword',
+        durability: 95,
+        enhancement: 14
+      };
+
+      const item = enhancer.fail(original);
+      expect(item).toEqual(expected);
+      //console.log(item);
+    });
+    it('subtract durability by 10 if enhancement more than 15,', () => {
+      const original2 = {
+        name: 'sword',
+        durability: 100,
+        enhancement: 17
+      };
+      const expected2 = {
+        name: 'sword',
+        durability: 90,
+        enhancement: 16
+      };
+
+      const item2 = enhancer.fail(original2);
+      expect(item2).toEqual(expected2);
+      //console.log(item2);
+    });
+
+    it('subtract enhancement by 1 if enhancement is more than 16', () => {
+      const original3 = {
+        name: 'sword',
+        durability: 100,
+        enhancement: 17
+      };
+      const expected3 = {
+        name: 'sword',
+        durability: 90,
+        enhancement: 16
+      };
+
+      const wand = {
+        name: 'wand',
+        durability: 100,
+        enhancement: 20
+      };
+
+      expect(enhancer.fail({ enhancement: 17 }).enhancement).toEqual(16);
+      const item3 = enhancer.fail(original3);
+      //expect(item3).toEqual(expected3);
+      //console.log(item3);
+    });
+  });
+});
